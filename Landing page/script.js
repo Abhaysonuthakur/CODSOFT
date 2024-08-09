7d9d38720bc866583a1e57708e11f0d8258508ad
Abhay Sonu Thakur <123862026+Abhaysonuthakur@users.noreply.github.com>
2024-08-09 20:22:58 +0530

    Add files via upload

---
+++ Landing page/script.js
@@ -0,0 +1,5 @@
+// Toggle Menu
+document.getElementById("nav-toggle").addEventListener("click", function () {
+  this.classList.toggle("active");
+  document.getElementById("nav-links").classList.toggle("show");
+});
